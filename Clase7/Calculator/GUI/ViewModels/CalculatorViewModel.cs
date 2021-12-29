38e038d3ec45ba979bed8fef28714036aa8a6f38
Emiliano Gallo (clients) <emilianogallo@hotmail.com>
2021-12-29 12:15:52 -0300

    Clase 7

---
+++ Clase7/Calculator/GUI/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Business.Models;
+using Business.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.ViewModels
+{
+    class CalculatorViewModel
+    {
+        private Boolean _firstValue;
+        private CalculatorWithHistory _calculator;
+
+        public CalculatorViewModel()
+        {
+            this._calculator = new CalculatorWithHistory();
+            this._firstValue = true;
+        }
+
+        //calc.Add(4);
+        //OperationBase add = new AddOperation(3.3);
+        //calc.Add(add);
+
+        public double CalculateResult()
+        {
+            return this._calculator.Do();
+        }
+
+        public void AddValue(double value)
+        {
+            if (this._firstValue)
+            {
+                this._firstValue = false;
+                this._calculator.Add(value);
+            }
+            else
+            {
+                // Obtener la última operación y asignarle el valor ingresado
+                this._calculator.AddValueToLastOperation(value);
+            }
+        }
+
+        public void AddOperation(OperationBase op)
+        {
+            this._calculator.Add(op);
+        }
+    }
+}
---
+++ Clase7/Calculator/GUI/Views/CalculatorView.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Business.Operations;
+using GUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI.Views
+{
+    public partial class CalculatorView : Window
+    {
+        private CalculatorViewModel _vm;
+        private Dictionary<string, OperationBase> _operations;
+
+        public CalculatorView()
+        {
+            InitializeComponent();
+            this._vm = new CalculatorViewModel();
+            DataContext = this._vm;
+
+            this._operations = new Dictionary<string, OperationBase>();
+            this._operations.Add("+", new AddOperation());
+            this._operations.Add("-", new SubtractOperation());
+            this._operations.Add("X", new MultiplyOperation());
+            this._operations.Add("/", new DivideOperation());
+        }
+
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            double result = this._vm.CalculateResult();
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+
+            int value = int.Parse(button.Content.ToString());
+            this._vm.AddValue(value);
+           // 8
+           //2
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            string key = button.Content.ToString();
+
+            OperationBase op = this._operations[key];
+            this._vm.AddOperation(op);
+            //+
+        }
+    }
+}
